9ee6a54112ce954422746586e1a34ea331c461b7
Dmast00 <obed.hinojosa@outlook.com>
2023-10-12 20:39:06 -0600

    ejercicos

---
+++ Leetcode/Concatenation_Of_array/ConcatenationOfArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Concatenation_Of_array
+{
+    public  class ConcatenationOfArray
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            int n = nums.Length;
+            int[] ans = new int[2*n];
+            //List<int> ans = new List<int>();
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                ans[i] = nums[i];
+                ans[i+n] = nums[i];
+            }
+
+
+
+            return ans;
+        }
+    }
+}
--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using Leetcode.Concatenation_Of_array;
+
+ConcatenationOfArray getConcatenation = new ConcatenationOfArray();
+int[] nums = {1, 3, 2, 1};
+
+
+
+Console.WriteLine(string.Join("\n",getConcatenation.GetConcatenation(nums)));
