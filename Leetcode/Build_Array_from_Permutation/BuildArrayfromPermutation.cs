e5ec9954702b18b541b3b5c69156e27d1f33c58f
Dmast00 <obed.hinojosa@outlook.com>
2023-10-12 20:48:34 -0600

    2

---
+++ Leetcode/Build_Array_from_Permutation/BuildArrayfromPermutation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Build_Array_from_Permutation
+{
+    public class BuildArrayfromPermutation
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            int n = nums.Length;
+            int[] ans = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int item = nums[i];
+                ans[i] = nums[item];
+            }
+            return ans;
+        }
+    }
+}
--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,11 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using Leetcode.Build_Array_from_Permutation;
 using Leetcode.Concatenation_Of_array;
 
-ConcatenationOfArray getConcatenation = new ConcatenationOfArray();
-int[] nums = {1, 3, 2, 1};
+
+#region objects
+    ConcatenationOfArray getConcatenation = new ConcatenationOfArray();
+    BuildArrayfromPermutation buildArrayfrom = new BuildArrayfromPermutation();
+#endregion
+
+
+#region variables
+    int[] nums = {1, 3, 2, 1};
+    
+#endregion
 
 
 
-Console.WriteLine(string.Join("\n",getConcatenation.GetConcatenation(nums)));
+
+
+Console.WriteLine(string.Join("\n",buildArrayfrom.BuildArray(nums)));
